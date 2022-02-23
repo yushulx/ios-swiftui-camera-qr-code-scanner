d6d17a89231f5139ed90214f2c282800965b1465
yushulx <lingxiao1002@gmail.com>
2022-02-23 19:17:04 +0800

    Initial Commit

---
+++ qrscanner/qrscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrscannerApp.swift
+//  qrscanner
+//
+//  Created by xiao on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct qrscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
